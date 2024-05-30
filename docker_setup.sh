b7cfa700c5e7c70ac146f0650866a6328673d80c
Yifan Chen <chenyifan@wustl.edu>
2024-05-29 21:57:13 -0500

    created pseudo-modular codebase for model dev

--- docker_setup.sh
+++ docker_setup.sh
@@ -12,18 +12,24 @@ cp -r "$dataset_folder/" "$final_dataset_folder/"
 
 echo "Starting Jupyter notebooks"
 
-if [ $(docker ps -a -f name=TimeSeriesModelExpriment | wc -l) -gt 1 ]; then
-    echo "Container TimeSeriesModelExpriment already exists."
-    docker start TimeSeriesModelExpriment
-    docker exec -it TimeSeriesModelExpriment /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks \
+# Check if the container is already running
+if [ "$(docker inspect -f '{{.State.Running}}' $container_name 2>/dev/null)" == "true" ]; then
+    echo "Container $container_name is already running."
+    docker exec -it $container_name /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks \
+    --ip='*' --port=$port --no-browser --allow-root
+elif [ "$(docker inspect -f '{{.State.Status}}' $container_name 2>/dev/null)" == "exited" ]; then
+    echo "Container $container_name exists but is not running."
+    docker start $container_name
+    docker exec -it $container_name /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks \
     --ip='*' --port=$port --no-browser --allow-root
 else
-    docker run -i --name=TimeSeriesModelExpriment \
+    echo "Creating and starting new container $container_name."
+    docker run -i --name=$container_name \
     -v $(PWD)/notebooks:/opt/notebooks -t \
     -p $port:$port continuumio/anaconda3 bin/bash \
     -c "/opt/conda/bin/conda install jupyter -y --quiet && \
         /opt/conda/bin/conda install -c conda-forge hyperopt=0.2.5 -y --quiet && \
+        /opt/conda/bin/conda install -c conda-forge ipywidgets -y --quiet && \
         mkdir -p /opt/notebooks && \
-        /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks \
-        --ip='*' --port=$port --no-browser --allow-root"
+        PYTHONWARNINGS='ignore' /opt/conda/bin/python -Xfrozen_modules=off /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=$port --no-browser --allow-root"
 fi
